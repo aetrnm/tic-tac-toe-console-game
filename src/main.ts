f8c9773b34e96f504317d01315c2c3c6c6848d5c
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-05-10 09:28:38 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Game from "./Game/Game.js";
+import Player from "./Player/Player.js";
 
 import { clearTerminal } from "./tools/clearTerminal.js";
 import { askNames } from "./tools/askNames.js";
@@ -6,10 +7,12 @@ import { askNames } from "./tools/askNames.js";
 clearTerminal();
 console.log("Welcome to the Tic Tac Toe game!");
 
-askNames().then((names) => {
-  const [player1, player2] = names;
+let player1: Player, player2: Player;
+
+await askNames().then((players) => {
+  [player1, player2] = players;
   player1.setOpponent(player2);
   player2.setOpponent(player1);
-
-  new Game(player1, player2).startGame();
 });
+
+new Game(player1, player2).startGame();
