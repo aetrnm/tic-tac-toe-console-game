b8a6b45d21f09166d58190ce5cc0f973490902b2
Oleg Gnat <oleggnatkovskii@gmail.com>
2022-05-13 10:01:43 +0200

    refactor

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -1,17 +1,27 @@
 import Player from "../Player/Player.js";
 import GameField from "../GameField/GameField.js";
+import askNames from "../tools/askNames.js";
 
 export default class Game implements IGame {
   player1: Player;
   player2: Player;
   gameField: GameField;
-  startGame(): void {
+  async startGame(): Promise<void> {
+    let player1: Player, player2: Player;
+
+    await askNames().then((players) => {
+      [player1, player2] = players;
+      player1.setOpponent(player2);
+      player2.setOpponent(player1);
+    });
+
+    this.player1 = player1;
+    this.player2 = player2;
+
     this.player1.turn(this.gameField);
   }
 
-  constructor(player1: Player, player2: Player) {
-    this.player1 = player1;
-    this.player2 = player2;
+  constructor() {
     this.gameField = new GameField();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import Game from "./Game/Game.js";
-import Player from "./Player/Player.js";
 
 import clearTerminal from "./tools/clearTerminal.js";
-import askNames from "./tools/askNames.js";
 
 clearTerminal();
 console.log("Welcome to the Tic Tac Toe game!");
 
-let player1: Player, player2: Player;
-
-await askNames().then((players) => {
-  [player1, player2] = players;
-  player1.setOpponent(player2);
-  player2.setOpponent(player1);
-});
-
-new Game(player1, player2).startGame();
+new Game().startGame();
